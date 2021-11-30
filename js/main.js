483b93fc0ed9e091164878430b248aa1b705f743
alternatorbob <cbogdan.nastase@gmail.com>
2021-11-30 23:15:34 +0100

    working grid

--- js/main.js
+++ js/main.js
@@ -8,25 +8,45 @@ let titles = [
   'Title number three',
   'And one last one',
 ]
-let arr = Array.from(document.getElementsByClassName('subContainer'))
+const arr = Array.from(document.getElementsByClassName('subContainer'))
 addNumbers()
-addTitles()
-let arr2 = Array.from(document.getElementsByClassName('title'))
+// addTitles()
+const arr2 = Array.from(document.getElementsByClassName('title'))
+const arr3 = Array.from(document.getElementsByClassName('box'))
 
 for (let i = 0; i < arr.length; i++) {
   // console.log(arr)
   arr[i].addEventListener('mouseover', () => {
     arr[i].style.flex = '50'
-    removeTitles(i)
+    currentTab = i
+    // removeTitles(i)
   })
 }
 
 for (let i = 0; i < arr.length; i++) {
   arr[i].addEventListener('mouseout', () => {
     arr[i].style.flex = '1'
+    // removeAllTitles()
     // addTitles()
+    // hasTitles()
+    currentTab = -1
   })
   // arr[i] = contentBackup[i]
+}
+for (let i = 0; i < arr3.length; i++) {
+  arr3[i].addEventListener('mouseover', () => {
+    arr3[i].style.flex = '30'
+    // currentTab = i
+    // removeTitles(i)
+  })
+}
+
+for (let i = 0; i < arr3.length; i++) {
+  arr3[i].addEventListener('mouseout', () => {
+    arr3[i].style.flex = '1'
+    // currentTab = i
+    // removeTitles(i)
+  })
 }
 
 document.getElementById('menu').addEventListener('mouseover', () => {
@@ -55,11 +75,22 @@ function addTitles() {
     myDiv.classList.add('title')
     myDiv.innerText = titles[i]
     arr[i].append(myDiv)
+    // console.log(arr[i].childNodes)
+    // console.log(arr[i].parentElement)
+    if (arr[i].innerHTML.includes('section')) arr[i].append(myDiv)
+    // console.log(arr[i].innerHTML)
+    // console.log(arr[i].innerHTML.includes('section'))
+    // if(arr[i].innerHTML.contains('class="title"')) {
+    // console.log('has title')
   }
 }
 
 function hasTitles() {
-  for (let i = 0; i < arr.length; i++) {}
+  arr.forEach((e) => {
+    // console.log(e.parentElement.childNodes)
+    // if (e.parentElement.childNodes.classList.contains('title'))
+    //   console.log('has titles')
+  })
 }
 
 function removeTitles(i) {
@@ -67,5 +98,10 @@ function removeTitles(i) {
     if (j !== i) {
       arr2[j].innerText = ''
     }
+  }
+}
+function removeAllTitles(i) {
+  for (let j = 0; j < arr2.length; j++) {
+    arr2[j].innerText = ''
   }
 }

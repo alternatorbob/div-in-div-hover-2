cb2c72e3155f9115db17a5add0b8c5ec3cc87ed0
alternatorbob <cbogdan.nastase@gmail.com>
2021-11-30 22:13:04 +0100

    working titles

--- js/main.js
+++ js/main.js
@@ -1,61 +1,71 @@
 let currentBox
 let boxes = document.getElementsByClassName('box')
-let currentId
+let currentTab = -1
 
-let arr = [].slice.call(boxes)
+let titles = [
+  'Mycelium compared to concrete',
+  'Durability and Beauty',
+  'Title number three',
+  'And one last one',
+]
+let arr = Array.from(document.getElementsByClassName('subContainer'))
+addNumbers()
+addTitles()
+let arr2 = Array.from(document.getElementsByClassName('title'))
 
-arr.forEach((element) => {
-  element.addEventListener('mouseover', () => {
-    currentId = element.getAttribute('id').charAt(3)
-    for (let i = 0; i < arr.length; i++) {
-      // console.log(arr[i].getAttribute('id'));
-      /*
-      {
-        arr.forEach((e) => {
-          if (!arr[i].getAttribute('id').includes(`${i}`))
-            e.classList.add('hidden')
-        })
-      }
-      */
-      // arr.forEach((e) => {
-      //   e.classList.add('hidden')
-      // })
-      // if (element.getAttribute('id') !== currentId)
-      //   element.classList.add('hidden')
-    }
+for (let i = 0; i < arr.length; i++) {
+  // console.log(arr)
+  arr[i].addEventListener('mouseover', () => {
+    arr[i].style.flex = '50'
+    removeTitles(i)
   })
-})
-
-arr.forEach((element) => {
-  element.addEventListener('mouseout', () => {
-    currentId = element.getAttribute('id')
-    for (let i = 0; i < arr.length; i++) {
-      arr.forEach((e) => {
-        e.classList.remove('hidden')
-      })
-      // if (element.getAttribute('id') !== currentId)
-      //   element.classList.add('hidden')
-    }
-  })
-})
-/*
-function hide() {
-  let currentId = element.getAttribute('id')
-
-  // for(i=0; i< arr.length<i++)
-  // {
-  //   if(element.getAttribute("id") !== currentId) element.classList.add("hidden");
-
-  // }
 }
 
-function unhide() {
-  let currentId = element.getAttribute('id')
-
-  // for(i=0; i< arr.length<i++)
-  // {
-  //   if(element.getAttribute("id") !== currentId) element.classList.remove("hidden");
-
-  // }
+for (let i = 0; i < arr.length; i++) {
+  arr[i].addEventListener('mouseout', () => {
+    arr[i].style.flex = '1'
+    // addTitles()
+  })
+  // arr[i] = contentBackup[i]
 }
-*/
+
+document.getElementById('menu').addEventListener('mouseover', () => {
+  console.log('MENUUUUUU')
+  let myDiv = document.getElementsByClassName('menu-content')[0]
+  console.log(myDiv)
+
+  myDiv.classList.remove('hidden')
+  myDiv.style.visibility = 'visible'
+  myDiv.style.transform = 'translate3d(0, 0, 0);'
+})
+
+function addNumbers() {
+  for (let i = 0; i < arr.length; i++) {
+    const myDiv = document.createElement('div')
+    myDiv.classList.add('number')
+    myDiv.innerText = i + 1
+    arr[i].prepend(myDiv)
+  }
+}
+
+function addTitles() {
+  for (let i = 0; i < arr.length; i++) {
+    const myDiv = document.createElement('div')
+    myDiv.id = `section-${i}`
+    myDiv.classList.add('title')
+    myDiv.innerText = titles[i]
+    arr[i].append(myDiv)
+  }
+}
+
+function hasTitles() {
+  for (let i = 0; i < arr.length; i++) {}
+}
+
+function removeTitles(i) {
+  for (let j = 0; j < arr2.length; j++) {
+    if (j !== i) {
+      arr2[j].innerText = ''
+    }
+  }
+}

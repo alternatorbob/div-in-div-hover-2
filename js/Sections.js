e172ebd0b6bf05a6cc4c81bbfe1fb1a243c35e11
alternatorbob <cbogdan.nastase@gmail.com>
2021-12-15 16:47:51 +0100

    title divs generated

---
+++ js/Sections.js
@@ -0,0 +1,62 @@
+export const SECTIONS = [
+  {
+    title: 'Intro',
+    colour: '#E1E1E1 [ grey ]',
+    content: [
+      {
+        subtitle: 'About',
+        text: 'This site is about mycelium',
+        images: ['img-path1', 'img-path2'],
+      },
+      {
+        subtitle: 'How to Use the Website',
+        text: 'To use the site hover in the cases, discover as much info as possible',
+        images: ['img-path1', 'img-path2'],
+      },
+    ],
+  },
+  {
+    title: 'Growth',
+    colour: '#B79588 [ brown ]',
+    content: [
+      {
+        subtitle: 'subtitle',
+        text: 'Lorem Ipsum',
+        images: ['img-path1', 'img-path2'],
+      },
+    ],
+  },
+  {
+    title: 'Advantages',
+    colour: '#809E8C [ green ]',
+    content: [
+      {
+        subtitle: 'subtitle',
+        text: 'Lorem Ipsum',
+        images: ['img-path1', 'img-path2'],
+      },
+    ],
+  },
+  {
+    title: 'Formal Studies',
+    colour: '#70589E [ purple ]',
+    content: [
+      {
+        subtitle: 'subtitle',
+        text: 'Lorem Ipsum',
+        images: ['img-path1', 'img-path2'],
+      },
+    ],
+  },
+  {
+    title: 'Recycling',
+    colour: '#F09FC3 [ pink ]',
+    content: [
+      {
+        subtitle: 'subtitle',
+        text: 'Lorem Ipsum',
+        images: ['img-path1', 'img-path2'],
+      },
+    ],
+  },
+]
--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
+import { SECTIONS } from './Sections.js'
+
+let sections = []
+
 let currentTab = -1
+
+let activeSection = 'formal-studies'
+
 let isTitle = true
 let scrollStarted,
   wasClicked = false
 
-const sections = ['growth', 'advantages', 'formal-studies', 'recycling']
+// const sections = ['growth', 'advantages', 'formal-studies', 'recycling']
 
 let myTitles = [
   'Mycelium compared to concrete',
@@ -15,27 +22,46 @@ let myTitles = [
 let pointerX,
   pointerY = -1
 
-for (let i = 0; i < sections.length; i++) {
-  let val = sections[i]
-  switch (val) {
-    case 'growth':
-      console.log('growth')
-      break
-    case 'advantages':
-      console.log('advantages')
-      break
-    case 'formal-studies':
-      console.log('formal-studies')
-      break
-    case 'recycling':
-      console.log('recycling')
-      break
+const logSections = () => {
+  for (let i = 0; i < sections.length; i++) {
+    let val = sections[i]
+    switch (val) {
+      case 'growth':
+        console.log('growth')
+        break
+      case 'advantages':
+        console.log('advantages')
+        break
+      case 'formal studies':
+        console.log('formal studies')
+        break
+      case 'recycling':
+        console.log('recycling')
+        break
+    }
   }
 }
 
-function updateSection() {
-  
+SECTIONS.map((project, idx) => {
+  const title = document.createElement('section')
+  title.id = `${project.title.replace(/\s+/g, '-').toLowerCase()}`
+  title.classList.add('title-section', 'deactive')
+  title.innerHTML = project.title
+  title.style.background = `${project.colour.substring(0, 7)}`
+  document.querySelector('.wrapper').appendChild(title)
+  sections.push(`${project.title.replace(/\s+/g, '-').toLowerCase()}`)
+})
+
+for (let i = 0; i < sections.length; i++) {
+  if (sections[i].includes(activeSection)) {
+    const curDiv = document.querySelector(`#${activeSection}`)
+    // document.querySelector(`#${activeSection}`).style.border = '1px solid red'
+    curDiv.classList.add('active')
+    curDiv.style.border = '1px solid red'
+  }
 }
+
+function updateSection() {}
 
 const subCont = Array.from(document.getElementsByClassName('subContainer'))
 addNumbers()
@@ -44,100 +70,6 @@ const titles = Array.from(document.getElementsByClassName('title'))
 const boxes = Array.from(document.getElementsByClassName('box'))
 
 const curSec = document.getElementsByClassName('title-section')[0]
-
-document.querySelector('#growth').addEventListener('click', (e) => {
-  curSec.style.flex = '0 1 0%'
-  curSec.style.transition = '.4s'
-  curSec.style.fontSize = '20px'
-  for (let i = 0; i < boxes.length; i++) {
-    const e = boxes[i]
-    e.classList.remove('hidden')
-  }
-  wasClicked = true
-  setTimeout(startScroll, 1500)
-  pointerCheck()
-})
-
-function startScroll() {
-  console.log('scroll started')
-
-  for (let i = 0; i < subCont.length; i++) {
-    // console.log('Width:  ' + subCont[i].offsetWidth)
-    // console.log('Height: ' + subCont[i].offsetHeight)
-    // console.log('bounding: ' + subCont[i].getBoundingClientRect())
-
-    for (var key in subCont[i]) {
-      if (typeof subCont[0][key] !== 'function') {
-        // console.log(`${key} : ${subCont[0][key]}`)
-
-        console.log(subCont[1].getBoundingClientRect().x)
-        console.log(subCont[1].getBoundingClientRect().y)
-
-        // console.log('pointerX: ' + pointerX)
-        // console.log('pointerY: ' + pointerY)
-        // console.log('offsetTop:' + subCont[0].offsetTop)
-        // console.log('offsetLeft:' + subCont[0].offsetLeft)
-        // console.log('offsetWidth:' + subCont[0].offsetWidth)
-        // console.log('offsetHeight:' + subCont[0].offsetHeight)
-      }
-      if (
-        pointerX >= subCont[1].offsetLeft &&
-        pointerX <=
-          subCont[1].getBoundingClientRect().x + subCont[1].offsetWidth
-      )
-        subCont[1].style.flex = '50'
-      currentTab = 1
-      // removeContent(i)
-      /*
-       &&
-        pointerY >= subCont[1].offsetHeight &&
-        pointerY <= subCont[1].offsetTop
-        */
-    }
-
-    subCont[i].addEventListener('mouseover', (e) => {
-      if (scrollStarted) {
-        subCont[i].style.flex = '50'
-        currentTab = i
-        removeContent(i)
-      }
-    })
-  }
-
-  for (let i = 0; i < subCont.length; i++) {
-    subCont[i].addEventListener('mouseout', () => {
-      subCont[i].style.flex = '1'
-      addContent(currentTab)
-      currentTab = -1
-    })
-  }
-}
-
-function pointerCheck(e) {
-  pointerX = (e || event).clientX
-  pointerY = (e || event).clientY
-  if (document.documentElement.scrollTop > 0) {
-    pointerY = pointerY + document.documentElement.scrollTop
-  }
-  console.log(pointerX, pointerY)
-  document.onmousemove = (event) => {
-    if (
-      pointerX + 5 == event.clientX ||
-      pointerX - 5 == event.clientX ||
-      pointerY + 5 == event.clientY ||
-      (pointerY - 5 == event.clientY && !scrollStarted)
-    ) {
-      // console.log(pointerX, pointerY)
-      // console.log('eventX: ' + event.clientX, 'eventY: ' + event.clientY)
-      console.log('mouse has been moved')
-      scrollStarted = true
-    }
-  }
-  if (scrollStarted) startScroll()
-}
-
-const myWrapper = document.getElementsByClassName('wrapper')[0]
-const mainContainer = document.querySelector('main.container')
 
 function addNumbers() {
   for (let i = 0; i < subCont.length; i++) {
